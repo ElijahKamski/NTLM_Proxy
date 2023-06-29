034cf3385c914fd1db039f625a2cd53f80865cf0
Iaroslav Khripkov <yarikkhripkov@gmail.com>
2023-06-29 22:40:40 +0300

    deleted useless file

--- testing/trash.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def main():
-    response = requests.get('http://google.com', proxies={"http": f"http://127.0.0.1:{5865}"})
-    print(response)
-
-if __name__=='__main__':
-    main()
